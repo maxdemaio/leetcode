0d0ccdc6d122f0f1e16448b3d40e29e386f3340e
maxdemaio <maxdman321@gmail.com>
2023-06-10 23:42:19 -0400

    add notes to topkfreq

--- java-problems/src/main/java/array/TopKFrequentElements.java
+++ java-problems/src/main/java/array/TopKFrequentElements.java
@@ -4,36 +4,59 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
+
+/*
+Approach:
+The algorithm uses a HashMap to count the frequency of each element in the input array.
+It then maintains a PriorityQueue of size k to store the k most frequent elements,
+allowing efficient retrieval of the desired elements based on their frequencies.
+
+Time complexity:
+The time complexity of the approach is O(nlog(k)),
+where n is the number of elements in the input array nums
+and k is the desired number of most frequent elements.
+Counting the frequency takes O(n) time,
+while maintaining and extracting the k most frequent elements takes O(nlog(k)) and O(klog(k)) time, respectively. The overall time complexity is O(nlog(k)) due to these operations, ensuring efficient computation for large inputs.
+
+Space complexity:
+The space complexity is O(n) due to the HashMap used to store the frequency count,
+which can have up to n distinct elements in the worst case.
+The additional space used by the PriorityQueue
+and the output array of size k is negligible compared to the input size,
+making the space required for the HashMap the primary factor determining the space complexity.
+
+Extra notes:
+ */
 public class TopKFrequentElements {
-    // get frequency counts in hashmap??
-    // then just get the highest ones
     public int[] topKFrequent(int[] nums, int k) {
-        // frequency map
-        Map<Integer, Integer> myMap = new HashMap<>();
-        for (int num: nums) {
-            Integer key = Integer.valueOf(num);
-            if (myMap.containsKey(key)) {
-                Integer count = myMap.get(key); // Retrieve current value
-                myMap.put(key, count + 1); // Increment and update value
-            } else {
-                myMap.put(key, 0);
-            }
-        }
-        // Use a min-heap to keep track of top k frequent elements
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> myMap.get(a) - myMap.get(b));
-        for (int key : myMap.keySet()) {
-            minHeap.offer(key); // Add key to the min-heap
-            if (minHeap.size() > k) {
-                minHeap.poll(); // Remove the element with the smallest frequency
-            }
+        int[] res = new int[k]; // Array to store the k most frequent elements
+        HashMap<Integer, Integer> map = new HashMap<>(); // HashMap to count the frequency of each element
+
+        // Count the frequency of each element using the HashMap
+        for (int num : nums)
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+        // Create a PriorityQueue to store entries from the HashMap based on their frequency
+        // The higher frequencies will have higher priority in the PriorityQueue
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
+                (a, b) -> a.getValue() - b.getValue()
+        );
+
+        // Add entries from the HashMap to the PriorityQueue
+        // Keep the size of the PriorityQueue equal to k by removing the entry with the smallest frequency
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.add(entry);
+            if (pq.size() > k)
+                pq.poll();
         }
 
-        // Convert the min-heap to an array
-        int[] res = new int[k];
-        for (int i = 0; i < k; i++) {
-            res[i] = minHeap.poll();
+        // Extract the k most frequent elements from the PriorityQueue and store them in the array
+        int i = k;
+        while (!pq.isEmpty()) {
+            res[--i] = pq.poll().getKey();
         }
 
-        return res;
+        return res; // Return the array containing the k most frequent elements
     }
+
 }
