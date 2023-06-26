3956908e4e6423bbe104e599018eb17ce114a4f0
maxdemaio <maxdman321@gmail.com>
2023-06-26 13:23:28 -0400

    fin merge sorted lls

--- python-problems/linkedlist/mergetwosortedlists.py
+++ python-problems/linkedlist/mergetwosortedlists.py
@@ -3,11 +3,17 @@ from linkedlist.util import ListNode
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        curr1 = list1
-        curr2 = list2
-        while curr1 is not None and curr2 is not None:
-            print(curr1.val)
-            print(curr2.val)
-            curr1 = curr1.next
-            curr2 = curr2.next
-        return None
+        cur = head = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2, cur = list2.next, list2
+
+        # If there are any leftover nodes, assign the rest of the list
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+
+        return head.next
--- python-problems/linkedlist/tests/test_mergetwosortedlists.py
+++ python-problems/linkedlist/tests/test_mergetwosortedlists.py
@@ -1,7 +1,5 @@
-from typing import *
 from linkedlist.mergetwosortedlists import Solution
 from linkedlist.util import Util
-
 
 def test_mergeTwoLists():
     util = Util()
@@ -14,8 +12,16 @@ def test_mergeTwoLists():
 
     # Test case 1
     merged_linkedList = solution.mergeTwoLists(test_linkedlist1, test_linkedlist2)
-    #assert Util.linked_list_to_list(merged_linkedList) == [1, 1, 2, 3, 4], "Linked lists not merged correctly"
+    assert util.linked_list_to_list(merged_linkedList) == [1, 1, 2, 3, 4, 4], "Linked lists not merged correctly"
 
+    list3 = [2, 2, 4]
+    list4 = [1, 8, 9]
+    test_linkedlist3 = util.setup_linked_list(list3)
+    test_linkedlist4 = util.setup_linked_list(list4)
+
+    # Test case 2
+    merged_linkedList = solution.mergeTwoLists(test_linkedlist3, test_linkedlist4)
+    assert util.linked_list_to_list(merged_linkedList) == [1, 2, 2, 4, 8, 9], "Linked lists not merged correctly"
 
 
 
