6d5d58ab384b0a84a21ae49f32d0175593057419
maxdemaio <maxdman321@gmail.com>
2023-08-02 19:09:57 -0400

    fix

--- java-problems/src/main/java/bitManipulation/SumOfTwoIntegers.java
+++ java-problems/src/main/java/bitManipulation/SumOfTwoIntegers.java
@@ -16,9 +16,9 @@ public class SumOfTwoIntegers {
     public int getSum(int a, int b) {
         while (b != 0) {
             int tmp = (a & b) << 1;
-            a = (a & b);
+            a = (a ^ b);
             b = tmp;
         }
-        return a;   
+        return a;
     }
 }
