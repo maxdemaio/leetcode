3f352df7011f61afb1df79de8253aee48f85ed9b
maxdemaio <maxdman321@gmail.com>
2023-08-02 18:53:54 -0400

    finish sum of two ints

---
+++ java-problems/src/test/java/BitManipulationTests.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import bitManipulation.SumOfTwoIntegers;
+import linkedList.RemoveNthNodeFromEndOfList;
+import org.junit.jupiter.api.Test;
+
+public class BitManipulationTests {
+
+    @Test
+    void testSumOfTwoIntegers() {
+        assertEquals(-1, new SumOfTwoIntegers().getSum(-2, 1));
+        assertEquals(0, new SumOfTwoIntegers().getSum(-1, 1));
+        assertEquals(4, new SumOfTwoIntegers().getSum(2, 2));
+        assertEquals(3, new SumOfTwoIntegers().getSum(1, 2));
+    }
+}
