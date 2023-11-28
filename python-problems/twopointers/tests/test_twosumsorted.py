4b1c6ed566f54720ba8a3a13cba881c9a9fc8b33
maxdemaio <maxdman321@gmail.com>
2023-11-27 20:06:32 -0500

    fix ci/cd

--- python-problems/twopointers/tests/test_twosumsorted.py
+++ python-problems/twopointers/tests/test_twosumsorted.py
@@ -2,12 +2,12 @@ from twopointers.twosumsorted import Solution
 
 
 def test_twoSum():
-    solution = Solution(),
+    solution = Solution()
 
-    # input = [2, 7, 11, 15]
-    # target = 9
-    # assert solution.twoSum(input, target) == [1, 2]
+    input = [2, 7, 11, 15]
+    target = 9
+    assert solution.twoSum(input, target) == [1, 2]
 
-    input = [5, 25, 75]
-    target = 100
-    assert solution.twoSum(input, target) == [2, 3]
+    input = [2,3,4]
+    target = 6
+    assert solution.twoSum(input, target) == [1, 3]
