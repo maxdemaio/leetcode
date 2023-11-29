80a8c3f5228afb12412548ed148799edf919eb6d
maxdemaio <maxdman321@gmail.com>
2023-11-28 19:03:38 -0500

    small fixes

--- python_problems/linkedlist/linkedlistcycle.py
+++ python_problems/linkedlist/linkedlistcycle.py
@@ -1,16 +1,17 @@
 from typing import Optional
 from linkedlist.util import ListNode
 
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if head is None:
             return False
-        
+
         tortoise = head
         hare = head.next
 
         while tortoise != hare:
-            if hare == None or hare.next == None:
+            if hare is None or hare.next is None:
                 return False
             tortoise = tortoise.next
             hare = hare.next.next
